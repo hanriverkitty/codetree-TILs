2d8a2567c27207002a208773bc8117f9afe2e2b1
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-22 06:59:08 +0900

    Title: 정수 선언하고 뺄셈, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 231122/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 231122/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -2,5 +2,5 @@ public class Main {
     public static void main(String[] args) {
         int a = 97;
         int b =13;
-        System.out.prinf("%d - %d = %d",a,b,a-b); }
+        System.out.printf("%d - %d = %d",a,b,a-b); }
 }
