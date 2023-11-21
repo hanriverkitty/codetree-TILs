d0882effa1a52834988193145d01b8e348f9dd71
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-21 19:10:54 +0900

    Title: 한줄 출력, Time: 76ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 231121/한줄 출력/print-one-line.java
+++ 231121/한줄 출력/print-one-line.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let\'s go LeebrodCode!")
+        System.out.println("Let\'s go LeebrodCode!");
     }
 }
