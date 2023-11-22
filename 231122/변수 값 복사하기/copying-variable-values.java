38d8c8251d8eea69ebe5586b3d7e7d07f43ac848
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-22 10:49:26 +0900

    Title: 변수 값 복사하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231122/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a=b=c;
+        System.out.println(a+" "+b+" "+c);
+    }
+}
