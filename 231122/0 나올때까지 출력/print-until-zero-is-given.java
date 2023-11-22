cce4fbbed720038a5fbca43ff97e73caee556ff1
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-22 13:25:40 +0900

    Title: 0 나올때까지 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231122/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(;;){
+            int a = sc.nextInt();
+            if(a==0){
+                break;
+            }
+            System.out.println(a);
+        }
+    }
+}
