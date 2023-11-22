7c237b539bfa796232e0b4059770ec2040290e57
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-22 12:20:43 +0900

    Title: 날짜 변경하여 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231122/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[]a = sc.next().split("-");
+        System.out.println("%s.%s.%s",a[2],a[0],a[1]);
+            }
+}
