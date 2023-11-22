1093b70751cc43a229f207348b96afeaec19159f
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-22 12:21:53 +0900

    Title: 날짜 변경하여 출력 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 231122/날짜 변경하여 출력 2/print-date-with-different-format-2.java
+++ 231122/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[]a = sc.next().split("-");
-        System.out.println("%s.%s.%s",a[2],a[0],a[1]);
+        String[] a = sc.next().split("-");
+        System.out.printf("%s.%s.%s",a[2],a[0],a[1]);
             }
 }
