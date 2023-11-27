4e43e70771eeccf3d519563907193b927fdcbb4f
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-27 17:16:19 +0900

    Title: Date to Date, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231127/Date to Date/date-to-date.java
+++ 231127/Date to Date/date-to-date.java
@@ -9,6 +9,10 @@ public class Main {
         int d2 = sc.nextInt();
         int sum1=0;
         int sum2=0;
+        if (m1==m2 && d1==d2){
+            System.out.println("1");
+            return;
+        }
         for (int i=0;i<m2;i++){
             if (i<m1){
                 sum1+=arr[i];
