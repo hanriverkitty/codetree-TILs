2164222b313ed66deeb240a073aa16491a73202e
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-11-27 17:20:51 +0900

    Title: Date to Date, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231127/Date to Date/date-to-date.java
+++ 231127/Date to Date/date-to-date.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
+        int[] arr = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
