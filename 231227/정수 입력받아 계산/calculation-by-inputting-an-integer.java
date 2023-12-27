4b31caf74a392f0ac7f8fb3aa0ec10d6230c25d1
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-12-27 16:02:38 +0900

    Title: 정수 입력받아 계산, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 231227/정수 입력받아 계산/calculation-by-inputting-an-integer.java
+++ 231227/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -4,6 +4,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.printf("%d",a*2+3)
+        System.out.printf("%d",a*2+3);
     }
 }
