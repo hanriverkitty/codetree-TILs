8b942b08434a462506b5de21e7cc56aec7c90eff
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2023-12-27 19:46:20 +0900

    Title: 실수 입력받아 계산 2, Time: 258ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231227/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        a = a+1.5;
+        System.out.printf("%.2f",a);
+        // 여기에 코드를 작성해주세요.
+    }
+}
