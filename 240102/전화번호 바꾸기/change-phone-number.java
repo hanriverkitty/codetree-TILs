a3d61134560e2ced9acf7211ea363abda1d5d5be
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-01-02 11:32:29 +0900

    Title: 전화번호 바꾸기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240102/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str[] = sc.nextLine().split("-");
+        System.out.println(str[0]+"-"+str[2]+"-"+str[1]);
+    }
+}
