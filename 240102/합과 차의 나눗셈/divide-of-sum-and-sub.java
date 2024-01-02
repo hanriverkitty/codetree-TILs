d50dcd129f59746686657bccac757ee7d9cc4304
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-01-02 11:45:05 +0900

    Title: 합과 차의 나눗셈, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240102/합과 차의 나눗셈/divide-of-sum-and-sub.java
+++ 240102/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        double a = sc.nextInt();
+        double b = sc.nextInt();
         System.out.printf("%.2f",(a+b)/(a-b));
     }
 }
