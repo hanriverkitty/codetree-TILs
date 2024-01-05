ad134678e360a6a73e864e74b354f045cd2b86bf
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-01-05 18:26:07 +0900

    Title: 학점 계산기, Time: 147ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240105/학점 계산기/credit-calculator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=sc.nextDouble();
+        }
+        System.out.println(sum);
+        if(sum>=4.0){
+            System.out.println("Perfect");
+        }else if(sum>=3.0){
+            System.out.println("Good");
+        }else{
+            System.out.println("Poor");
+        }
+        
+    }
+}
