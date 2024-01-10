e7902a935d9084024fc5ff772e63512275dceabf
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-01-10 18:30:27 +0900

    Title: 배열 채우고 출력하기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240110/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String arr[] = new String[10];
+        arr = sc.nextLine().split(" ");
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
