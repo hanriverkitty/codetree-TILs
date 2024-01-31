39f40ebf25270e805652bc952e7a1a724a481a7a
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-01-31 22:09:40 +0900

    Title: 부분 문자열의 개수, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240131/부분 문자열의 개수/number-of-substrings.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        int cnt=0;
+        for(int i=0;i<a.length()-1;i++){
+        
+                if (b.length()==1 && (a.charAt(i)==(b.charAt(0)))){
+                    cnt++;
+                    continue;
+                }
+                if((a.charAt(i)==(b.charAt(0)))&&(a.substring(i,i+b.length()).equals(b))){
+                    cnt++;
+                }
+            
+        }
+        System.out.println(cnt);
+    }
+}
