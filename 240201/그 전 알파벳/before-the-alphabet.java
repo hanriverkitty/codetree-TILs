31002e7e3fde61e3a26b30ac8e1b6e918ef206d5
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-01 17:00:20 +0900

    Title: 그 전 알파벳, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240201/그 전 알파벳/before-the-alphabet.java
+++ 240201/그 전 알파벳/before-the-alphabet.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char a = sc.next().charAt(0);
+        if(a=='a'){
+            System.out.print('z');
+        }
+        else{
         System.out.print((char)(a-1));
+        }
     }
 }
