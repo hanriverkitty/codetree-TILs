30f02291805f2df6e832ea9b3ff0860759d23304
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-01 15:59:52 +0900

    Title: 두 번째를 첫 번째로, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240201/두 번째를 첫 번째로/second-to-first.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        char k = arr[1];
+        char l = arr[0];
+        for(int i=0;i<str.length();i++){
+            if (arr[i]==k){
+                arr[i]=l;
+            }
+            System.out.print(arr[i]);
+        }
+        
+    }
+}
