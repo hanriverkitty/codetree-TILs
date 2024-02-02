b40bcef393cf9bb1927f0f7397daa3d91e2617fd
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-02 10:53:20 +0900

    Title: 움직이는 블록, Time: 3010ms, Memory: 15MB, Status: Time Limit Exceed - CodeTree

---
+++ 240202/움직이는 블록/moving-block.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        int cnt=0;
+        while(true){
+            int max=0;
+            int min=9999999;
+            int max_idx=0;
+            int min_idx=0;
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]>max){
+                    max = arr[i];
+                    max_idx=i;
+                }
+                if(arr[i]<min){
+                    min = arr[i];
+                    min_idx = i;
+                }
+                
+                
+            }
+            if (max==min){
+                System.out.println(cnt);
+                break;
+            }
+            arr[max_idx]-=1;
+            arr[min_idx]+=1;
+            cnt++;
+            
+        }
+    }
+}
