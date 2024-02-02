b676acbffd5e6c97d5016e6824f14f6557308fd3
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-02 11:15:27 +0900

    Title: 움직이는 블록, Time: 322ms, Memory: 20MB, Status: Passed - CodeTree

--- 240202/움직이는 블록/moving-block.java
+++ 240202/움직이는 블록/moving-block.java
@@ -4,35 +4,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        int sum = 0;
         for (int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
+            sum+=arr[i];
         }
-        int cnt=0;
-        while(true){
-            int max=0;
-            int min=9999999;
-            int max_idx=0;
-            int min_idx=0;
-            for(int i=0;i<arr.length;i++){
-                if(arr[i]>max){
-                    max = arr[i];
-                    max_idx=i;
-                }
-                if(arr[i]<min){
-                    min = arr[i];
-                    min_idx = i;
-                }
-                
-                
+        sum=(int)sum/n;
+        int ans = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<sum){
+                ans += sum-arr[i];
             }
-            if (max==min){
-                System.out.println(cnt);
-                break;
-            }
-            arr[max_idx]-=(int)(max-min)/2;
-            arr[min_idx]+=(int)(max-min)/2;
-            cnt+=(int)(max-min)/2;
-            
         }
+        System.out.println(ans);
     }
 }
