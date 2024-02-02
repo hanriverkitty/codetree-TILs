f0c61b61f648eef11eb8351293bd5952c1110324
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-02 17:19:51 +0900

    Title: 전부 포함하는 선분, Time: 294ms, Memory: 15MB, Status: Passed - CodeTree

--- 240202/전부 포함하는 선분/all-inclusive-segment.java
+++ 240202/전부 포함하는 선분/all-inclusive-segment.java
@@ -19,6 +19,7 @@ public class Main {
         for(int i=0;i<n;i++){
             min = Math.min(min,arr[i][0]);
         }
+        
         a = (arr[n-2][1]-min);
         
         Arrays.sort(arr,(k,l)->k[0]-l[0]);
@@ -27,7 +28,10 @@ public class Main {
             max = Math.max(max,arr[i][1]);
         }
         b = (max-arr[1][0]);
-        
+        if(max==arr[0][1]){
+            System.out.print(a+b-(arr[0][1]-arr[0][0]));
+        }else{
         System.out.print(Math.min(a,b));
+        }
     }
 }
