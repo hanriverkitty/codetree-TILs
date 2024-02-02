9a8c1c24e006fbec5aaf4271cabb8e0357ac87a3
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-02 10:15:48 +0900

    Title: 줄 세우기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240202/줄 세우기 2/line-up-students-2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Student implements Comparable<Student>{
+    int h;
+    int w;
+    int num;
+    public Student(int h,int w,int i){
+        this.h=h;
+        this.w=w;
+        this.num = i;
+    }
+    @Override
+    public int compareTo(Student a){
+        if(a.h==this.h){
+               return this.w-a.w;
+            }else{
+               return this.h-a.h;
+            }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Student[] arr = new Student[n];
+        for (int i=0; i<n;i++){
+            arr[i] = new Student(sc.nextInt(),sc.nextInt(),i+1);
+        }
+        Arrays.sort(arr);
+        for(Student a:arr){
+            System.out.println(a.h+" "+a.w+" "+a.num);
+        }
+    }
+}
