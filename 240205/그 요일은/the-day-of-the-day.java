6ee1a22390c849fb4911a17f2a0d9ead6cc6a3df
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-05 13:26:50 +0900

    Title: 그 요일은, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240205/그 요일은/the-day-of-the-day.java
+++ 240205/그 요일은/the-day-of-the-day.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/* import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
@@ -50,5 +50,56 @@ public class Main {
                 
             }
         
+    }
+}
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static int numOfDays(int m, int d) {
+        // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+        int[] days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        
+        // 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+        for(int i = 1; i < m; i++)
+            totalDays += days[i];
+        
+        // m월의 경우에는 정확이 d일만 있습니다.
+        totalDays += d;
+        
+        return totalDays;
+    }
+    
+    public static int numOfDay(String s) {
+        // 간단한 비교를 위해 요일을 숫자로 나타내줍니다.
+        if(s.equals("Mon")) return 0;
+        else if(s.equals("Tue")) return 1;
+        else if(s.equals("Wed")) return 2;
+        else if(s.equals("Thu")) return 3;
+        else if(s.equals("Fri")) return 4;
+        else if(s.equals("Sat")) return 5;
+        return 6;
+    }
+    
+    public static int ans;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A = sc.next();
+
+        int startDate = numOfDays(m1, d1);
+        int endDate = numOfDays(m2, d2);
+        int k = numOfDay(A);
+        int ans = (int)(endDate-startDate-k)/7;
+        if(ans+k>=7){
+            ans++;
+        }
+        System.out.println(ans);
     }
 }
