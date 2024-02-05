80e0398cab1156632906a631b157f9e09957bc67
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-05 13:21:14 +0900

    Title: 그 요일은, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240205/그 요일은/the-day-of-the-day.java
+++ 240205/그 요일은/the-day-of-the-day.java
@@ -10,12 +10,18 @@ public class Main {
         String date = sc.next();
         int ans = 0;
         int day = 0;
+
+        //요일 기록
         List<String> arr1 = new ArrayList<>(Arrays.asList("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"));
-        int count = arr1.indexOf(date);
         List<Integer> arr = new ArrayList<>(Arrays.asList(1,3,5,7,8,10,12));
+
+        //월요일 보다 A요일이 얼마나 더 앞에 있는지 확인
+        int count = arr1.indexOf(date);
+        
+        //m2 d2에 있는 월요일 탐색
             while(true){
                 if(m1==m2 && d1==d2){
-                    
+                    //day가 A요일의 인덱스보다 크면 A요일이 한번더 포함될 수 있다
                     if (day>=count){
                         ans++;
                     }
