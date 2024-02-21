a745a992b31e5f3d34ef46d08222047798bd116b
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-21 14:45:22 +0900

    Title: X 달리기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240221/X 달리기/x-run.java
+++ 240221/X 달리기/x-run.java
@@ -1,21 +1,38 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력
         int n = sc.nextInt();
-        int min = n;
-        for (int i=1;i<5001;i++){
-            if(i*(i+1)-i>n){
-                continue;
+
+        int time = 0;
+        int dist = n;
+        int speed = 1;
+
+        while(true) {
+            dist -= speed;
+            time++;
+
+            if(leftDist == 0)
+                break;
+
+            
+            // 속도를 1 더 높이면, 시그마 speed+1 까지 이동가능 
+            if(dist >= (speed + 1) * (speed + 2) / 2)
+                speed++;
+            
+            
+            // 속도를 유지하면, 시그마 speed 만큼 이동 가능
+            else if(dist >= speed * (speed + 1) / 2) {
+                
             }
-            if((n-(i*(i+1)-i))/i>0){
-                int div = (n-(i*(i+1)-i))/i;
-                min = Math.min(n-(i*(i+1)-i+div*i)+2*i-1+div,min);
-            }else{
-            min = Math.min(n-(i*(i+1)-i)+2*i-1,min);
-            }
+
+            // 위 둘을 만족하지 못하면 반드시 속도를 줄여야한다
+            else
+                speed--;
         }
-        System.out.println(min);
+        
+        System.out.print(time);
     }
 }
