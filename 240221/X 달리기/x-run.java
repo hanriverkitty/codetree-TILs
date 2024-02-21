3f43d2696bbe1de9a5339c37631cde3e5e7458f5
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-02-21 14:32:52 +0900

    Title: X 달리기, Time: 108ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240221/X 달리기/x-run.java
+++ 240221/X 달리기/x-run.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int min = Integer.MAX_VALUE;
+        int min = n;
         for (int i=1;i<5001;i++){
             if(i*(i+1)-i>n){
                 continue;
