536394c5d8a3619b3e03d1114a4807d62e98c6d9
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-05 18:29:14 +0900

    Title: 최고의 33위치, Time: 183ms, Memory: 10MB, Status: Passed - CodeTree

--- 240305/최고의 33위치/best-place-of-33.java
+++ 240305/최고의 33위치/best-place-of-33.java
@@ -11,17 +11,22 @@ public class Main {
             }
         }
         int max=0;
+        // 격자점 선택
         for (int i=0;i<n;i++){
             for (int j=0;j<n;j++){
+
+                // 탐색범위가 격자를 벗어나는지
                 if (i+2<=n-1 && j+2<=n-1){
                     int cnt=0;
-                for (int k=0;k<3;k++){
-                    for (int l=0;l<3;l++){
-                    if (arr[i+k][j+l]==1){
-                        cnt++;
+
+                    // 3*3 안에 1이 있는 경우
+                    for (int k=0;k<3;k++){
+                        for (int l=0;l<3;l++){
+                        if (arr[i+k][j+l]==1){
+                            cnt++;
+                        }
+                      }
                     }
-                }
-                }
                 max = Math.max(cnt,max);
                 }
                
