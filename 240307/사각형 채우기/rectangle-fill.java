518555dc6dc59e435a34a778c8cb1fdc6d607f3e
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-07 15:52:42 +0900

    Title: 사각형 채우기, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240307/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[1001];
+        arr[0]=0;
+        arr[1]=1;
+        arr[2]=2;
+        for(int i=3;i<1001;i++){
+            arr[i]=arr[i-2]+arr[i-1];
+        }
+        System.out.print(arr[n]);
+    }
+}
