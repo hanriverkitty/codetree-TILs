515d84d88023c86fcab168b399094d13a42d0fd0
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-07 15:54:28 +0900

    Title: 사각형 채우기, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240307/사각형 채우기/rectangle-fill.java
+++ 240307/사각형 채우기/rectangle-fill.java
@@ -8,7 +8,7 @@ public class Main {
         arr[1]=1;
         arr[2]=2;
         for(int i=3;i<1001;i++){
-            arr[i]=arr[i-2]+arr[i-1];
+            arr[i]=(arr[i-2]+arr[i-1])%10007;
         }
         System.out.print(arr[n]);
     }
