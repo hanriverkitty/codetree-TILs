2cd423aa1fea926e6a8e82e584b7585a425c964b
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-08 12:00:30 +0900

    Title: 최대 증가 부분 수열, Time: 278ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240308/최대 증가 부분 수열/longest-increasing-subsequence.java
+++ 240308/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -12,7 +12,7 @@ public class Main {
         dp[0]=0;
         for(int i=1;i<=n;i++){            
             for(int j=0;j<i;j++){
-                if(dp[j]==-13) continue;
+                
                 if(arr[j]<=arr[i]){
                     dp[i] = Math.max(dp[i],dp[j]+1);
                 }
