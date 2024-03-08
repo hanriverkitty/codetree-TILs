3bacda933889ac21b5c1dc45b66050f08a19b6d2
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-08 12:22:06 +0900

    Title: 동전 거슬러주기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240308/동전 거슬러주기/coin-change.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    static int MAX = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] coins = new int[n];
+        for(int i=0;i<n;i++){
+            coins[i] = sc.nextInt();
+        }
+        int[] dp = new int[m+1];
+        Arrays.fill(dp,MAX);
+        dp[0] = 0;
+        for(int i=1;i<m+1;i++){
+            for(int j=0;j<n;j++){
+                if(coins[j]<=i){
+                    dp[i] = Math.min(dp[i],dp[i-coins[j]]+1);
+                }
+            }
+        }
+        
+        if(dp[m]!=MAX){
+            System.out.print(dp[m]);
+        }else{
+            System.out.print(-1);
+        }
+        
+    }
+}
