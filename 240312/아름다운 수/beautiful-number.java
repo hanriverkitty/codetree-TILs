317432314e2ce3e7253d907d6728bf004e6ef8bc
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-12 15:58:12 +0900

    Title: 아름다운 수, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240312/아름다운 수/beautiful-number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    static String[] arr = new String[]{"1","22","333","4444"};
+    static ArrayList<String> strings = new ArrayList<>();
+    static int n;
+    public static void make(String a){
+        if (a.length()==n){
+            strings.add(a);
+            return;
+        }
+        if (a.length()<n){
+            for (String str:arr){
+                a = a+str;
+                make(a);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i=0;i<4;i++){
+            make(arr[i]);
+        }
+        System.out.print(strings.size());
+    }
+}
