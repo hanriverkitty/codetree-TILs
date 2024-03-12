047b06237b3a2565e6cee6a9bbfe6dd232ecd4c4
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-12 14:53:48 +0900

    Title: 행복한 수열의 개수, Time: 263ms, Memory: 20MB, Status: Passed - CodeTree

--- 240312/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240312/행복한 수열의 개수/number-of-happy-sequence.java
@@ -16,7 +16,10 @@ public class Main {
             int cnt=1;
             int temp1=-1;
             int cnt1=1;
-            for(int j=0;j<n;j++){                
+
+            //행 탐색
+            for(int j=0;j<n;j++){   
+                // 전꺼와 다르면 값을 바꾸고 그동안 셋던 연속된 개수 초기화             
                 if (arr[i][j] != temp){
                     temp = arr[i][j];
                     cnt=1;
@@ -24,12 +27,16 @@ public class Main {
                 else 
                     cnt++;                    
                 
+                // 연속된 개수가 m을 넘으면 더 이상 찾을 필요가 없으므로 탐색종료
                 if(cnt==m){
                     answer++;
                     break;
                 }
             }
-            for(int k=0;k<n;k++){                
+
+            //열 탐색
+            for(int k=0;k<n;k++){
+                // 전꺼와 다르면 값을 바꾸고 그동안 셋던 연속된 개수 초기화                
                 if (arr[k][i] != temp1){
                     temp1 = arr[k][i];
                     cnt1=1;
@@ -37,6 +44,7 @@ public class Main {
                 else 
                     cnt1++;                    
                 
+                // 연속된 개수가 m을 넘으면 더 이상 찾을 필요가 없으므로 탐색종료
                 if (cnt1==m){
                     answer++;
                     break;
