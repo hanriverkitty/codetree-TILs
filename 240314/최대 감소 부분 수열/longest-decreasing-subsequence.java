26add24331c08014977da7d03826eaeb90be913b
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-14 09:45:45 +0900

    Title: 최대 감소 부분 수열, Time: 373ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240314/최대 감소 부분 수열/longest-decreasing-subsequence.java
+++ 240314/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -9,7 +9,7 @@ public class Main {
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(arr[i]<arr[j]){
                     dp[i] = Math.max(dp[i],dp[j]+1);
