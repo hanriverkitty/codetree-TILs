809bc6c2a63f5744c67cdfeda57d7741b66074aa
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-17 02:23:26 +0900

    Title: 삼각형 컨베이어 벨트, Time: 201ms, Memory: 12MB, Status: Passed - CodeTree

--- 240317/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
+++ 240317/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -14,6 +14,7 @@ public class Main {
         int cnt=0;
         for(int i=0;i<3*n;i++){
             System.out.print(dq.pollFirst()+" ");
+            cnt++;
             if (cnt==n){
                 System.out.println();
                 cnt=0;
