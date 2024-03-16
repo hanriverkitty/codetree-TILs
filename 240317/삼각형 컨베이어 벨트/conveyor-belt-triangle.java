112fd615c3b04550352abe264f337a25b8397866
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-17 02:19:56 +0900

    Title: 삼각형 컨베이어 벨트, Time: 3099ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240317/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Deque <Integer> dq = new ArrayDeque<>();
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        for(int i=0;i<3*n;i++){
+            dq.addLast(sc.nextInt());
+        }
+        for(int i=0;i<t;t++){
+            dq.addFirst(dq.pollLast());
+        }
+        int cnt=0;
+        for(int i=0;i<3*n;i++){
+            System.out.print(dq.pollFirst()+" ");
+            if (cnt==n){
+                System.out.println();
+                cnt=0;
+            }
+        }
+    }
+}
