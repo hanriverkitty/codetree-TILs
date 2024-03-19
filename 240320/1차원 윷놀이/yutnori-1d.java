d1b327e5aad07661012288479e7e4ffe6775f17b
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-20 08:32:13 +0900

    Title: 1차원 윷놀이, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240320/1차원 윷놀이/yutnori-1d.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main {
+    public static int[] arr;
+    public static int[] toy;
+    public static int n;
+    public static int max;
+    public static int k;
+    public static int m;
+    public static void go(int time){
+        if(time==n){
+            max = Math.max(max,calc());
+            return;
+        }
+        for(int i=0;i<k;i++){
+            if(toy[i]>=m){
+                i++;
+                continue;
+            }
+            toy[i] = toy[i]+arr[time];
+            go(time+1);
+            toy[i] = toy[i]-arr[time];
+        }
+    }
+    public static int calc(){
+        int cnt=0;
+        for(int a:toy){
+            if(a>=m){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+        arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        toy = new int[k];
+        go(0);
+        System.out.print(max);
+    }
+}
