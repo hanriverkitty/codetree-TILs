b02945b2870c32ae9381cfcb5fcc18cd369ed6ae
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-20 08:34:58 +0900

    Title: 1차원 윷놀이, Time: 157ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240320/1차원 윷놀이/yutnori-1d.java
+++ 240320/1차원 윷놀이/yutnori-1d.java
@@ -34,7 +34,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        m = sc.nextInt();
+        m = sc.nextInt()-1;
         k = sc.nextInt();
         arr = new int[n];
         for(int i=0;i<n;i++){
