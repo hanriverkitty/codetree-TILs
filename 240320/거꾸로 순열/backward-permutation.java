3669dc92fba7644e2b9ce505a40c6c73cc1ab774
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-20 10:34:39 +0900

    Title: 거꾸로 순열, Time: 1071ms, Memory: 26MB, Status: Passed - CodeTree

--- 240320/거꾸로 순열/backward-permutation.java
+++ 240320/거꾸로 순열/backward-permutation.java
@@ -1,43 +1,25 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-
+import java.util.*;
 public class Main {
-    public static final int MAX_N = 8;
-    
-    // 변수 선언
-    public static int n;
-    public static boolean[] visited = new boolean[MAX_N + 1];
-    public static ArrayList<Integer> picked = new ArrayList<>();
-    
-    // 지금까지 선택한 수의 개수를 cnt라 했을 때
-    // 계속 탐색을 이어서 진행합니다.
-    public static void getPermutation(int cnt) {
-        // 모든 원소를 선택했을 때, 해당 순열을 출력합니다.
-        if(cnt == n) {
-            for(int i = 0; i < (int) picked.size(); i++)
-                System.out.print(picked.get(i) + " ");
+    static int n;
+    static List<Integer> arr = new ArrayList<>();
+    public static void cal(int time){
+        if(time==n){
+            for(int a:arr){
+                System.out.print(a+" ");
+            }
             System.out.println();
             return;
         }
-    
-        // 뒤에서부터 하나씩 원소를 선택합니다.
-        for(int i = n; i >= 1; i--) {
-            if(visited[i]) continue;
-            visited[i] = true;
-            picked.add(i);
-    
-            getPermutation(cnt + 1);
-    
-            visited[i] = false;
-            picked.remove(picked.size() - 1);
+        for(int i=n;i>=1;i--){
+            if (arr.contains(i)) continue;
+            arr.add(i);
+            cal(time+1);
+            arr.remove(arr.size()-1);
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 입력:
         n = sc.nextInt();
-
-        getPermutation(0);
+        cal(0);
     }
 }
