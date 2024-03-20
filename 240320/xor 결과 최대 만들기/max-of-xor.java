09067f0365397abe277f75a9ba46db4b2b4b765e
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-20 09:47:58 +0900

    Title: xor 결과 최대 만들기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240320/xor 결과 최대 만들기/max-of-xor.java
+++ 240320/xor 결과 최대 만들기/max-of-xor.java
@@ -1,31 +1,41 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
-    static List<Integer> num = new ArrayList<>();
-    static int n,m,max;
-    static int[] arr;
+    public static final int MAX_NUM = 20;
     
-    public static void back(int time,int idx,int val){
-        if(time==m){
-            max = Math.max(max,val);
+    // 전역 변수 선언:
+    public static int n, m;
+    public static int[] A = new int[MAX_NUM];
+    
+    public static int ans;
+    
+    public static void findMaxXor(int currIdx, int cnt, int currVal) {
+        if(cnt == m) {
+            ans = Math.max(ans, currVal);
             return;
         }
-        for(int i=idx;i<n;i++){
-            num.add(arr[i]);
-            back(time+1,i+1,val ^ arr[i]);
-            num.remove(num.size()-1);
-        }
-
+    
+        if(currIdx == n) 
+            return;
+    
+        // currIdx index 에 있는 숫자를 선택하지 않은 경우
+        findMaxXor(currIdx + 1, cnt, currVal);
+        
+        // currIdx index 에 있는 숫자를 선택한 경우
+        findMaxXor(currIdx + 1, cnt + 1, currVal ^ A[currIdx]);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 입력:
         n = sc.nextInt();
         m = sc.nextInt();
-        arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-        }
-        back(0,0,0);
-        System.out.println(max);
+        for(int i = 0; i < n; i++) 
+            A[i] = sc.nextInt();
+
+        findMaxXor(0, 0, 0);
+        
+        // 출력:
+        System.out.print(ans);
     }
 }
