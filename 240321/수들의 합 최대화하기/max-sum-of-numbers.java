bf641f6857155cd52521f9a1ae05d1cbe52dbdf6
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-21 18:14:18 +0900

    Title: 수들의 합 최대화하기, Time: 445ms, Memory: 12MB, Status: Passed - CodeTree

--- 240321/수들의 합 최대화하기/max-sum-of-numbers.java
+++ 240321/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -1,20 +1,22 @@
 import java.util.*;
 public class Main {
     public static int[][] arr;
-    public static int[][] visited;
     public static int n;
     public static int max;
     public static List<Integer> make = new ArrayList<>();
-    public static List<int[]> comb = new ArrayList<>();
     public static void color(int time){
         if(time==n){
             int sum=0;
+
+            //순서쌍 조합 생성 후 max 값 찾기
             for (int i=0;i<make.size();i++){
                 sum += arr[i][make.get(i)];
             }
             max = Math.max(sum,max);
             return;
         }
+
+        //순서쌍에서 두번째 수에 들어갈 중복되지 않은 조합 생성
         for(int i=0;i<n;i++){
             if (make.contains(i)) continue;
             make.add(i);
