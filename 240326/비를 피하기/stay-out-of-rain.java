6235cc1aedb7209af84f038740341e762474526d
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-26 13:37:34 +0900

    Title: 비를 피하기, Time: 390ms, Memory: 22MB, Status: Wrong Answer - CodeTree

--- 240326/비를 피하기/stay-out-of-rain.java
+++ 240326/비를 피하기/stay-out-of-rain.java
@@ -31,7 +31,6 @@ public class Main {
                     else if (!visited[mx][my] && arr[mx][my]==3){
                         visited[mx][my]=true;
                         answer[x][y]=temp[nx][ny]+1;
-                        arr[mx][my]=0;
                         return;
                     }
                 }
