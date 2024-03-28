4434deb2f9e1f552a7bda4bf9a8d4ebcad8b1ac1
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-03-28 16:16:41 +0900

    Title: 겹치지 않게 선분 고르기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240328/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.awt.Point;
+public class Main {
+    public static int n;
+    public static ArrayList<Point> arr = new ArrayList<>();
+    public static int max=0;
+    public static ArrayList<Point> comb = new ArrayList<>();
+    public static void search(int time,int next){
+        if(time==n){
+            max = Math.max(max,comb.size());
+        }
+        for(int i=next;i<n;i++){
+            if (duplicate(arr.get(i))){
+                comb.add(arr.get(i));
+                search(time+1,next+1);
+                comb.remove(comb.size()-1);
+            }
+        }
+    }
+    public static boolean duplicate(Point point){
+        for(int i=0;i<comb.size();i++){
+            if(!(comb.get(i).y<point.x)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i=0;i<n;i++){
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            arr.add(new Point(x1,x2));
+        }
+        Collections.sort(arr, new Comparator<Point>(){
+		@Override
+		public int compare(Point s1, Point s2){
+		return Integer.compare(s1.x, s2.x);
+        }
+        });
+        search(0,0);
+        System.out.println(max);
+    }
+}
