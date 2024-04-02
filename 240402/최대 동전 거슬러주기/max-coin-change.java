3aa7907f04c991ae2c33760f627294a9cb1264e7
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-04-02 16:34:10 +0900

    Title: 최대 동전 거슬러주기, Time: 150ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240402/최대 동전 거슬러주기/max-coin-change.java
+++ 240402/최대 동전 거슬러주기/max-coin-change.java
@@ -10,10 +10,14 @@ public class Main {
         }
         
         int[] dp = new int[m+1];
+        Arrays.fill(dp,Integer.MIN_VALUE);
         dp[0]=0;
         for(int i=1;i<m+1;i++){
             for(int j=0;j<n;j++){
-                dp[i] = Math.max(dp[i-coins[j]]+1,dp[i]);
+                if(dp[i]==Integer.MIN_VALUE) continue;
+                if(i-coins[j]>=0){
+                    dp[i] = Math.max(dp[i-coins[j]]+1,dp[i]);
+                }
             }
         }
         System.out.print(dp[m]>0 ? dp[m] : -1);
