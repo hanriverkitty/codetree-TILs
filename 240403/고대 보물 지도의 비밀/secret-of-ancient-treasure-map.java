06b9e261408fa38432de286839368c8f4717b2bd
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-04-03 14:04:24 +0900

    Title: 고대 보물 지도의 비밀, Time: 3042ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 240403/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ 240403/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -4,25 +4,31 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
+        int[] arr = new int[n+1];
+
+        for(int i=1;i<n+1;i++){
             arr[i] = sc.nextInt();
         }
+
         int[][] dp = new int[n+1][k+1];
-        
-        dp[0][0]=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<k;j++){
-                if(arr[i]>0){
-                    dp[i+1][j] = Math.max(arr[i],dp[i][j]+arr[i]);
+
+        for(int i=1;i<n+1;i++){
+                if(arr[i]>=0){
+                    for(int j=0;j<k+1;j++){
+                        dp[i][j] = Math.max(dp[i-1][j]+arr[i], dp[i][j]);
+                    }                   
                 }
-                else if(arr[i]<0){
-                    dp[i+1][j+1] = Math.max(arr[i],dp[i][j]+arr[i]);
-                }
-            }
+                else{
+                    for(int j=1;j<k+1;j++){
+                        dp[i][j] = Math.max(dp[i-1][j-1]+arr[i],dp[i][j]);
+                    }                    
+                }           
         }
+        int max=0;
         for(int i=0;i<n+1;i++){
-            System.out.println(Arrays.toString(dp[i]));
+            for(int j=0;j<k+1;k++){
+                max = Math.max(dp[i][j],max);
+            }
         }
     }
 }
