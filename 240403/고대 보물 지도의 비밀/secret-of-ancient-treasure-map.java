2510a8e3cf1aa035eb0fdff0aecb6d500c0168f2
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-04-03 14:06:36 +0900

    Title: 고대 보물 지도의 비밀, Time: 771ms, Memory: 36MB, Status: Passed - CodeTree

--- 240403/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ 240403/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -11,24 +11,22 @@ public class Main {
         }
 
         int[][] dp = new int[n+1][k+1];
-
+        int max =0;
         for(int i=1;i<n+1;i++){
                 if(arr[i]>=0){
                     for(int j=0;j<k+1;j++){
                         dp[i][j] = Math.max(dp[i-1][j]+arr[i], dp[i][j]);
+                        max = Math.max(dp[i][j],max);
                     }                   
                 }
                 else{
                     for(int j=1;j<k+1;j++){
                         dp[i][j] = Math.max(dp[i-1][j-1]+arr[i],dp[i][j]);
+                        max = Math.max(dp[i][j],max);
                     }                    
                 }           
         }
-        int max=0;
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<k+1;k++){
-                max = Math.max(dp[i][j],max);
-            }
-        }
+        
+        System.out.print(max);
     }
 }
